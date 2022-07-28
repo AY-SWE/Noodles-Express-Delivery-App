bae1f4401040bd125736e6a0a342b2305ce798c3
Andy Yang <yangandy1001@gmail.com>
2022-07-28 16:25:03 -0400

    added order schema to sanity dataset

---
+++ client/sanity/schemas/order.js
@@ -0,0 +1,41 @@
+export default {
+    name: "order",
+    title: "Order",
+    type: "document",
+    fields:[
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+            options:{
+                maxLength: 40
+            }
+        },
+        {
+            name: "phone",
+            title: "Phone",
+            type: "string",
+            options:{
+                maxLength: 15
+            }
+        },
+        {
+            name: "address",
+            title: "Address",
+            type: "string",
+            options:{
+                maxLength: 100
+            }
+        },
+        {
+            name: "total",
+            title: "Total",
+            type: "number"
+        },
+        {
+            name: "status",
+            title: "Status",
+            type: "number"
+        }
+    ]
+}
