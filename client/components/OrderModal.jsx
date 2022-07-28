85788dbdfc4bc2af59e862c4009761f66738719f
Andy Yang <yangandy1001@gmail.com>
2022-07-27 23:02:20 -0400

    continue working on order modal

--- client/components/OrderModal.jsx
+++ client/components/OrderModal.jsx
@@ -1,7 +1,10 @@
 import { Modal, useMantineTheme} from '@mantine/core';
+import css from "../styles/OrderModal.module.css";
 
 export default function OrderModal({opened, setOpened, paymentMethod}) {
     const theme = useMantineTheme();
+
+    const total = typeof window !== 'undefined' && localStorage.getItem('total')
     return(
         <Modal
         overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
@@ -11,7 +14,14 @@ export default function OrderModal({opened, setOpened, paymentMethod}) {
         onClose={()=> setOpened(null)}
       >
         {/* Modal content */}
-        fsdf
+        <form action="" className={css.formContainer}>
+            <input type="text" name='name' required placeholder='Name' />
+            <input type="text" name='phone' required placeholder='Phone Number' />
+            <textarea name="address"  rows={3}></textarea>
+
+            <span>You will pay <span>${total}</span> on delivery</span>
+            <button className='buttons' type='submit'>Place Order</button>
+        </form>
 
 
       </Modal>
